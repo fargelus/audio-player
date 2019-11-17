9cd803d85be05e59c6cb6cc74594d1a9d3c5f442
fargelus <ddraudred@gmail.com>
2019-11-17 08:17:19 +0300

    Codestyle fix

--- src/scripts/components/Playlist.jsx
+++ src/scripts/components/Playlist.jsx
@@ -10,25 +10,23 @@ const Playlist = props => {
       });
   }
 
-  const createCompositionItem = (item, index) => {
-    return (
-      <div className="composition" key={index}>
-        <div className="composition__info">
-          <small>{item.author}</small>
-          <div>{item.title}</div>
-        </div>
-
-        <div>
-          <button type="button"
-                  onClick={() => props.onChangeSong(item.src)}
-                  className="play-btn">
-            &#9658;
-          </button>
-          <div>{item.duration}</div>
-        </div>
+  const createCompositionItem = (item, index) => (
+    <div className="composition" key={index}>
+      <div className="composition__info">
+        <small>{item.author}</small>
+        <div>{item.title}</div>
       </div>
-    );
-  }
+
+      <div>
+        <button type="button"
+                onClick={() => props.onChangeSong(item.src)}
+                className="play-btn">
+          &#9658;
+        </button>
+        <div>{item.duration}</div>
+      </div>
+    </div>
+  );
 
   const compositions = createCompositionList();
   return (
